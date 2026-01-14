78ba81be464e80ebce3176e81686065c9b2fd2d1
Anton Balak <antonbalack3@gmail.com>
2026-01-14 15:56:35 +0100

    Add files via upload

---
+++ app/src/main/java/com/Rez1n/smartalarm/TimeLogic.kt
@@ -0,0 +1,23 @@
+package com.example.alarmclock
+
+import java.util.Calendar
+
+object TimeLogic {
+
+    fun calculateTriggerTime(hour: Int, minute: Int): Calendar {
+        val now = Calendar.getInstance()
+
+        val triggerTime = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, hour)
+            set(Calendar.MINUTE, minute)
+            set(Calendar.SECOND, 0)
+            set(Calendar.MILLISECOND, 0)
+        }
+
+        if (triggerTime.before(now)) {
+            triggerTime.add(Calendar.DAY_OF_YEAR, 1)
+        }
+
+        return triggerTime
+    }
+}
